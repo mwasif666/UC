09bdf1c2387eb8ff4021ca1b9cc44a4594946ebd
Muhammad Wasif <mwasif@renexusgroup.internal>
2024-03-05 13:08:19 +0500

    Uc

---
+++ jquery.js
@@ -0,0 +1,39 @@
+
+
+// About Slick Slider
+$(".slick-carousel").slick({
+  speed: 5000,
+  autoplay: true,
+  autoplaySpeed: 0,
+  cssEase: "linear",
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  infinite: true,
+  swipeToSlide: true,
+  centerMode: true,
+  focusOnSelect: true,
+  responsive: [
+    {
+      breakpoint: 750,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+  ],
+});
+
+
+
+// Initialize tooltips
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
